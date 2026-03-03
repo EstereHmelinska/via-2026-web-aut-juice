e3037627d81021e0e1336eaf798532f209dd65a0
EstereHmelinska <esterehmelinska3@gmail.com>
2026-03-03 13:41:28 +0200

    Added Cypress tests and page objects

--- cypress/pageObjects/homePage.js
+++ cypress/pageObjects/homePage.js
@@ -12,4 +12,36 @@ export class HomePage extends BasePage {
   static get meWantItButton() {
     return cy.get("[aria-label='dismiss cookie message']");
   }
+  static get accountButton() {
+    return cy.get("#navbarAccount");
+  }
+  static get loginButton() {
+    return cy.get("#navbarLoginButton");
+  }
+  static get userProfileButton() {
+    return cy.get("button[aria-label='Go to user profile']");
+  }
+  static get searchQueryButton() {
+    return cy.get("#searchQuery");
+  }
+  static get searchInputField() {
+    return cy.get("#mat-input-1");
+  }
+  static get productCards() {
+    return cy.get(
+      "[aria-label='Click for more information about the product']",
+    );
+  }
+  static get productCardInfo() {
+    return cy.get("[class*='mdc-dialog__content']");
+  }
+  static get closeButton() {
+    return cy.get("[class*='mat-mdc-outlined-button mat-unthemed']");
+  }
+  static get expandButton() {
+    return cy.get("[class='mat-expansion-panel-header mat-focus-indicator']");
+  }
+  static get reviewInfo() {
+    return cy.get("[class='review-row']");
+  }
 }
